9ee0a8016685a217f5ff4799464c13bec36aa49c
Carlos Emannoel <carlosemannoel2019@gmail.com>
2024-02-27 21:36:40 -0300

    refactor: removendo componentes antigos

--- src/screens/ListTravel/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const ListTravelStyled = styled.View`
-  align-items: "center";
-  justify-content: center;
-  padding: 0;
-  background-color: #fff;
-`;
--- src/screens/MyTravels/myTravels.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-
-export const ListTravelStyled = styled.View`
-  align-items: "center";
-  justify-content: center;
-  justify-content: flex-start;
-  padding: 0;
-  background-color: #fff;
-`;
